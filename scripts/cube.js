89d8597f0e4ec51d0d5fd080d84a79e88c8172e3
David Mojica <davidmojicav@gmail.com>
2023-12-21 11:57:39 -0500

    Cube

--- scripts/cube.js
+++ scripts/cube.js
@@ -1,17 +1,33 @@
 const scene = new THREE.Scene();
-const cam = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+// Cámara
+const cam = new THREE.PerspectiveCamera(55, 1, 0.1, 1000);
+cam.position.z = 5;
+
+// Renderizador
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setSize("50%", "50%");
 
-document.body.appendChild(renderer.domElement);
+const element = document.getElementById('element');
+element.appendChild(renderer.domElement);
 
-//Geometry
+// Cubo
 const geometry = new THREE.BoxGeometry();
 const material = new THREE.MeshBasicMaterial({ color: 0xfff });
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
-cam.position.z = 5;
+
+function handleResize() {
+    const newWidth = window.innerWidth;
+    const newHeight = window.innerHeight;
+
+    cam.aspect = newWidth / newHeight;
+    cam.updateProjectionMatrix();
+
+    renderer.setSize(newWidth, newHeight);
+}
+window.addEventListener('resize', handleResize);
 
 function animate(){
     requestAnimationFrame(animate);
@@ -19,4 +35,5 @@ function animate(){
     cube.rotation.y += 0.01;
     renderer.render(scene, cam);
 }
+handleResize();
 animate();
