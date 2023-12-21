b355d23ae99179be347cfe5833ca7b7e4ebd1b93
David Mojica <davidmojicav@gmail.com>
2023-12-21 09:41:52 -0500

    Schematic

--- scripts/elements.js
+++ scripts/elements.js
@@ -1,2 +1,20 @@
 const scene = new THREE.Scene();
 const cam = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+document.body.appendChild(renderer.domElement);
+
+//Geometry
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+cam.position.z = 5;
+
+function animate(){
+    requestAnimationFrame(animate);
+    renderer.render(scene, cam);
+}
+animate();
