bca668de3869c27014ee8593e5f3a85d0e1c9357
David Mojica <davidmojicav@gmail.com>
2023-12-22 10:40:42 -0500

    Perspective devepment

--- scripts/perspective.js
+++ scripts/perspective.js
@@ -1,12 +1,17 @@
 // Escena
 const scene = new THREE.Scene();
+//scene.background = new THREE.Color(0x666666);
 
 // Cámara
 const cam = new THREE.PerspectiveCamera(55, 1, 0.1, 1000);
-cam.position.z = 5;
+cam.position.z = 15;
+cam.position.y = -10;
+cam.rotation.y = -10;
+cam.rotation.x = 10;
+
 
 // Renderizador
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({alpha: true}); //alpha: transparencia
 renderer.setSize("50%", "50%");
 
 const element = document.getElementById('element');
@@ -17,6 +22,10 @@ const geometry = new THREE.BoxGeometry();
 const material = new THREE.MeshBasicMaterial({ color: 0xfff });
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
+
+//grid
+var grid = new THREE.GridHelper(100, 100); //numero de cuadrados que va a tener la malla
+scene.add(grid);
 
 // Función para manejar el cambio de tamaño del div
 function handleResize() {
