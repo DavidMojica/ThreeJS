ee15a618f9036b1abf2b83b01bec14ff62dee120
David Mojica <davidmojicav@gmail.com>
2023-12-22 11:23:00 -0500

    Shadows

--- scripts/perspective.js
+++ scripts/perspective.js
@@ -3,32 +3,35 @@ const scene = new THREE.Scene();
 //scene.background = new THREE.Color(0x666666);
 
 // Cámara
-const cam = new THREE.PerspectiveCamera(55, 1, 0.1, 1000);
+const cam = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 cam.position.z = 5;
-cam.position.y = -10;
-cam.rotation.x = 1;
+cam.position.y = -3;
+cam.rotation.x = .5;
 // cam.rotation.y = -10;
 
 // Renderizador
 const renderer = new THREE.WebGLRenderer({alpha: true}); //alpha: transparencia
 renderer.setSize("50%", "50%");
+renderer.shadowMap.enabled = true;
 
 const element = document.getElementById('element');
 element.appendChild(renderer.domElement);
 
 // Cubo
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial({ color: 0xfff });
+const geometry = new THREE.BoxGeometry(1,1,1);
+const material = new THREE.MeshStandardMaterial({ color: 0x00ff00 });
 const cube = new THREE.Mesh(geometry, material);
 cube.castShadow = true; // Generar sombras
-cube.position.set(1,2,2);
+cube.receiveShadow = true; // Permitir que el cubo reciba sombras
+cube.position.set(0,0,2);
 scene.add(cube);
 
 // LIGHT
-var light = new THREE.DirectionalLight(0xffffff, 1, 100);
-light.position.set(0,1,1);
-light.castShadow = true; // Generar sombras
+const light = new THREE.DirectionalLight(0xffffff, 1);
+light.position.set(-1, 1, 1);
+light.castShadow = true;
 scene.add(light);
+
 
 //grid
 // var grid = new THREE.GridHelper(100, 100); //numero de cuadrados que va a tener la malla
@@ -36,7 +39,7 @@ scene.add(light);
 
 // PLANE
 var planeGeometry = new THREE.PlaneGeometry(20,20,32,32);
-var planeMaterial = new THREE.MeshBasicMaterial({color: 0xff0000});
+var planeMaterial = new THREE.MeshStandardMaterial({color: 0xff0000});
 var plane = new THREE.Mesh(planeGeometry, planeMaterial);
 plane.receiveShadow = true; //Recibir sombras
 plane.position.set(0,0,0);
